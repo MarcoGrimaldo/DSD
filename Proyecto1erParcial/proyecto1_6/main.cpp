fc65f90ead2dc70e0546cd9fe6a09c1194e7a0b5
Marco Grimaldo <voskogrim@gmail.com>
2020-02-09 01:08:11 -0600

    Dibujo del Ortoedro

--- Proyecto1erParcial/proyecto1_6/main.cpp
+++ Proyecto1erParcial/proyecto1_6/main.cpp
@@ -5,11 +5,24 @@ using namespace std;
 
 int main( )
 {
-    Rectangulo rectangulo1(Coordenada(2,3),Coordenada(5,1));
-    double ancho, alto;
-    cout << "Calculando el área de un rectángulo dadas sus coordenadas en un plano cartesiano:\n";
-    rectangulo1.imprimeEsq();
-    cout << "El área del rectángulo es = " <<  rectangulo1.obtieneArea() << endl;
-   
+    cout << "=========== ORTOEDRO ===========\n\n";
+    cout << "          Y" << endl;
+    cout << "          ^" << endl;
+    cout << "          |" << endl;
+    cout << "          |" << endl;
+    cout << "          . - - - - - -(2)" << endl;
+    cout << "         /|            /|" << endl;
+    cout << "        / |           / |" << endl;
+    cout << "       . - - - - - - .  |" << endl;
+    cout << "       |  |          |  |" << endl;
+    cout << "       |  O - - - - -|- . - - -> X" << endl;
+    cout << "       | /           | /" << endl;
+    cout << "       |/            |/" << endl;
+    cout << "      (1)- - - - - - ." << endl;
+    cout << "      /" << endl;
+    cout << "     /" << endl;
+    cout << "    |/" << endl;
+    cout << "   Z" << endl;
+
     return 0;
 } 
