ef7fac709df50feb56e47e5bfd4a7097efba1dee
Marco Grimaldo <voskogrim@gmail.com>
2020-04-02 15:07:21 -0600

    Agregando practica 13 Confiable

---
+++ Practica13/Respuesta.h
@@ -0,0 +1,21 @@
+#ifndef RESPUESTA_H_
+#define RESPUESTA_H_
+
+#include "Mensaje.h"
+#include "SocketDatagrama.h"
+#include "PaqueteDatagrama.h"
+#include <netdb.h>
+#include <cstdlib>
+#include <cstring>
+
+class Respuesta{
+    public:
+        Respuesta(int pl);
+        struct mensaje *getRequest(void);
+        void sendReply(char *respuesta, char *ipCliente, int puertoCliente);
+    private:
+        SocketDatagrama *socketlocal;
+        unsigned int contRequest;
+};
+
+#endif
